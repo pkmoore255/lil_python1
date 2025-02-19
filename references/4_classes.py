cab4276b7deb4cf59e2db39c0b0b13c65cfc2433
pkmoore255 <pkconder255@gmail.com>
2025-02-19 12:03:32 -0500

    update dog and desk classes

--- references/4_classes.py
+++ references/4_classes.py
@@ -1,22 +1,43 @@
-# classes
+# classes, instances
 # blueprint / plans
 # 
 ##########################################################################
+import random
 
 # dog class
-
 class Dog:
     info = "Man's best friend"
 
+    def __init__(self, name):
+        print("I am alive")
+        self.lucky_number = random.randint(1,10)
+        self.name = name
+
 # print(Dog.info)
 
+# dog instance/object
+# dog1 = Dog("Fido")
+# dog2 = Dog("Toopka")
+
+# print(dog1.lucky_number, dog1.name) 
+# print(dog2.lucky_number, dog2.name)
 
 # desk class
 
 class Desk:
-    legs = 3
-    shelves = 5
-    surface = "flat"
-    color = "gray"
+    '''A furniture item designed to be a physical workspace.'''
+    
+def __init__(self, leg_count, shelf_count, surface_type, color):
+    self.leg_count = leg_count
+    self.shelf_count = shelf_count
+    self.surface_type = surface_type
+    self.color = color
 
-print (Desk.legs)
+
+# print(Desk.legs)
+my_desk = Desk(3, 5, "flat", "gray")
+
+    # legs = 3
+    # shelves = 5
+    # surface = "flat"
+    # color = "gray"
