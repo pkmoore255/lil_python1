0623bb86f68b1cf58025e85091e37dd6bfd14b48
pkmoore255 <pkconder255@gmail.com>
2025-02-19 09:32:00 -0500

    create desk class

---
+++ references/4_classes.py
@@ -0,0 +1,22 @@
+# classes
+# blueprint / plans
+# 
+##########################################################################
+
+# dog class
+
+class Dog:
+    info = "Man's best friend"
+
+# print(Dog.info)
+
+
+# desk class
+
+class Desk:
+    legs = 3
+    shelves = 5
+    surface = "flat"
+    color = "gray"
+
+print (Desk.legs)
